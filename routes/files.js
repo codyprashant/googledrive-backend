fd97448e0de83f590ad7b0dd574522d235e3662e
dreamloverss <49119019+dreamloverss@users.noreply.github.com>
2020-11-24 14:39:30 +0530

    Update files.js

--- routes/files.js
+++ routes/files.js
@@ -120,50 +120,6 @@ filesRoute.get("/getAllFiles", authorize, async (req, res) => {
   }
 });
 
-// const uploadsBusinessGallery = multer({
-// 	storage: multerS3({
-// 		s3: s3,
-// 		bucket: 'orionnewbucket',
-// 		acl: 'public-read',
-// 		key: function (req, file, cb) {
-// 			cb( null, path.basename( file.originalname, path.extname( file.originalname ) ) + '-' + Date.now() + path.extname( file.originalname ) )
-// 		}
-// 	}),
-// 	limits:{ fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
-// 	fileFilter: function( req, file, cb ){
-// 		checkFileType( file, cb );
-// 	}
-// }).array( 'galleryImage', 4 );
 
-// filesRoute.post('/multiple-file-upload', ( req, res ) => {
-// 	uploadsBusinessGallery( req, res, ( error ) => {
-// 		console.log( 'files', req.files );
-// 		if( error ){
-// 			console.log( 'errors', error );
-// 			res.json( { error: error } );
-// 		} else {
-// 			// If File not found
-// 			if( req.files === undefined ){
-// 				console.log( 'Error: No File Selected!' );
-// 				res.json( 'Error: No File Selected' );
-// 			} else {
-// 				// If Success
-// 				let fileArray = req.files,
-// 					fileLocation;
-// 				const galleryImgLocationArray = [];
-// 				for ( let i = 0; i < fileArray.length; i++ ) {
-// 					fileLocation = fileArray[ i ].location;
-// 					console.log( 'filenm', fileLocation );
-// 					galleryImgLocationArray.push( fileLocation )
-// 				}
-// 				// Save the file name into database
-// 				res.json( {
-// 					filesArray: fileArray,
-// 					locationArray: galleryImgLocationArray
-// 				} );
-// 			}
-// 		}
-// 	});
-// });
 
 module.exports = filesRoute;
